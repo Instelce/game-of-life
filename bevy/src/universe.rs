08848536a14dba66bd9112d828599590e529f8e2
Instelce <instelce@protonmail.com>
2024-05-27 20:46:02 +0200

    feat(bevy): fix bugs and add timer to tick

--- bevy/src/universe.rs
+++ bevy/src/universe.rs
@@ -1,6 +1,10 @@
+use std::time::Duration;
+
 use bevy::{
-    app::{Plugin, Update},
-    ecs::system::{ResMut, Resource}, reflect::List,
+    app::{Plugin, Startup, Update},
+    ecs::system::{Commands, Res, ResMut, Resource},
+    reflect::List,
+    time::{Time, Timer, TimerMode},
 };
 use rand::Rng;
 
@@ -11,6 +15,7 @@ pub struct UniversePlugin;
 impl Plugin for UniversePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.insert_resource(Universe::init(100, 100));
+        app.add_systems(Startup, setup_timer);
         app.add_systems(Update, tick);
     }
 }
@@ -20,6 +25,12 @@ pub struct Universe {
     pub width: u32,
     pub height: u32,
     pub cells: Vec<Vec<Cell>>,
+    pub generation_time: u64,
+}
+
+#[derive(Resource)]
+pub struct UniverseTimer {
+    timer: Timer,
 }
 
 impl Universe {
@@ -55,59 +66,79 @@ impl Universe {
             width,
             height,
             cells,
+            generation_time: 200
         }
     }
 
-    pub fn live_neighbor(&self, row: u32, col: u32) -> u8 {
+    pub fn live_neighbor(&self, x: u32, y: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().copied() {
-            for delta_col in [self.width - 1, 0, 1].iter().copied() {
-                let row = (delta_row + row) % self.width;
-                let col = (delta_col + col) % self.width;
-                count += match self.get_cell(col, row) {
-                    Some(cell) => cell.status as u8,
-                    None => 0
-                };
+        for delta_y in [self.height - 1, 0, 1].iter().copied() {
+            for delta_x in [self.width - 1, 0, 1].iter().copied() {
+                if delta_x == 0 && delta_y == 0 {
+                    continue;
+                }
+
+                let row = (delta_y + y) % self.height;
+                let col = (delta_x + x) % self.width;
+                count += self.get_cell(col, row).unwrap().status as u8;
             }
         }
         count
     }
 
     pub fn get_cell(&self, x: u32, y: u32) -> Option<&Cell> {
-        self.cells.get(y as usize).and_then(|row| row.get(x as usize))
+        self.cells
+            .get(y as usize)
+            .and_then(|row| row.get(x as usize))
+    }
+
+    pub fn next_generation(&mut self) {
+        let mut next_cells: Vec<Vec<Cell>> = Vec::new();
+
+        for y in 0..self.height {
+            let mut row_vec = Vec::new();
+
+            for x in 0..self.width {
+                match self.get_cell(x, y) {
+                    Some(cell) => {
+                        let cell_status = &cell.status;
+                        let live_neighbor = self.live_neighbor(x, y);
+
+                        let new_cell_status = match (cell_status, live_neighbor) {
+                            (CellStatus::Alive, x) if x < 2 => CellStatus::Dead,
+                            (CellStatus::Alive, 2) | (CellStatus::Alive, 3) => CellStatus::Alive,
+                            (CellStatus::Alive, x) if x > 3 => CellStatus::Dead,
+                            (CellStatus::Dead, 3) => CellStatus::Alive,
+                            (other, _) => other.clone(),
+                        };
+
+                        let new_cell = Cell::new(x as f32, y as f32, new_cell_status);
+
+                        row_vec.push(new_cell);
+                    }
+                    None => {}
+                };
+            }
+            next_cells.push(row_vec);
+        }
+
+        self.cells = next_cells;
     }
 }
 
-fn tick(mut universe: ResMut<Universe>) {
-    let mut next_cells: Vec<Vec<Cell>> = Vec::new();
+fn tick(mut universe: ResMut<Universe>, time: Res<Time>, mut universe_timer: ResMut<UniverseTimer>) {
+    universe_timer.timer.tick(time.delta());
 
-    for y in 0..universe.height {
-        let mut row_vec = Vec::new();
-
-        for x in 0..universe.width {
-
-            match universe.get_cell(x, y) {
-                Some(cell) => {
-                    let cell_status = &cell.status;
-                    let live_neighbor = universe.live_neighbor(x, y);
-
-                    let new_cell_status = match (cell_status, live_neighbor) {
-                        (CellStatus::Alive, x) if x < 2 => CellStatus::Dead,
-                        (CellStatus::Alive, 2) => CellStatus::Alive,
-                        (CellStatus::Alive, x) if x > 3 => CellStatus::Dead,
-                        (CellStatus::Dead, 3) => CellStatus::Alive,
-                        (other, _) => other.clone(),
-                    };
-
-                    let new_cell = Cell::new(x as f32, y as f32, new_cell_status);
-
-                    row_vec.push(new_cell);
-                }
-                None => {}
-            };
-        }
-        next_cells.push(row_vec);
+    if universe_timer.timer.finished() {
+        universe.next_generation();
     }
+}
 
-    universe.cells = next_cells;
+fn setup_timer(
+    mut commands: Commands,
+    universe: Res<Universe>,
+) {
+    commands.insert_resource(UniverseTimer {
+        timer: Timer::new(Duration::from_millis(universe.generation_time), TimerMode::Repeating)
+    })
 }
